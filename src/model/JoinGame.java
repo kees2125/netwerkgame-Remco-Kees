23bcf9af976658510e3c674a1c41572b64efaa51
Aareschluchtje <remcosannen@gmail.com>
2015-06-09 10:30:34 +0200

    Revert "Revert "Added some classed for menu en deleted some shit""
    This reverts commit cb33ac0dc3d8aa4acdee7a2b16fbe1e141be7ae2.

---
+++ src/model/JoinGame.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+
+public class JoinGame extends AbstractModel{
+
+	@Override
+	public void draw(Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
